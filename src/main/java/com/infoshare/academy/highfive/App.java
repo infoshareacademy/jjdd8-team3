c3f4ccbb97455ab7f5341817049fd2c79acf183a
Bartosz Dagiel <bartosz.dagiel@gmail.com>
2019-12-08 20:48:06 +0100

    Deleted comments

--- src/main/java/com/infoshare/academy/highfive/App.java
+++ src/main/java/com/infoshare/academy/highfive/App.java
@@ -3,14 +3,13 @@ package com.infoshare.academy.highfive;
 import com.infoshare.academy.highfive.MenuConsola.MainMenu;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import java.io.IOException;
-import java.text.ParseException;
 
 public class App {
     private static final Logger stdout = LoggerFactory.getLogger("CONSOLE_OUT");
 
-    public static void singletonInitialization() throws IOException, ParseException {
-        //HolidaysSingleton.getInstance().initFromFile("abc.json");
+    public static void singletonInitialization() throws IOException {
         HolidaysSingleton.getInstance().initFromURL("https://calendarific.com/api/v2/holidays?&api_key=c2ddb57bb630fc01911bbcd01ae5907afaaced8e058cc0f33a938f517c0321e3&country=PL&year=2019");
         HolidaysSingleton.getInstance().initSaveToFile("2019.json");
     }
