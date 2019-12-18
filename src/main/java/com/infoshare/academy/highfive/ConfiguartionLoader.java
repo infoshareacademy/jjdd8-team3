ad471e052a6ddae5bc87306c73473964c5f6f723
Marek <marek.wladyslaw.zajac@gmail.com>
2019-12-18 16:58:24 +0100

    complete handling configuration for file

--- src/main/java/com/infoshare/academy/highfive/ConfiguartionLoader.java
+++ src/main/java/com/infoshare/academy/highfive/ConfiguartionLoader.java
@@ -1,17 +1,36 @@
 package com.infoshare.academy.highfive;
 
+import com.infoshare.academy.highfive.employeemgmt.Employee;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.time.format.DateTimeFormatter;
+import java.util.Comparator;
 import java.util.Properties;
 
 public class ConfiguartionLoader {
 
-    private static Properties properties;
+    private static final Logger stdout = LoggerFactory.getLogger("CONSOLE_OUT");
 
-    static {
-        properties = initProperties();
+    private static final Comparator<Employee> COMPARATOR_ASC = (left, right) -> {
+        int compareResult = left.getSurname().compareTo(right.getSurname());
+        return compareResult != 0 ? compareResult : left.getFirstName().compareTo(right.getFirstName());
+    };
+
+    private static final Comparator<Employee> COMPARATOR_DESC = (left, right) -> {
+        int compareResult = left.getSurname().compareTo(right.getSurname());
+        return (-1)*(compareResult != 0 ? compareResult : left.getFirstName().compareTo(right.getFirstName()));
+    };
+    private static Properties properties = null;
+
+    private static Properties getProperties(){
+        if (properties == null){
+            properties = initProperties();
+        }
+        return properties;
     }
 
     private static Properties initProperties() {
@@ -25,12 +44,14 @@ public class ConfiguartionLoader {
     }
 
     public static DateTimeFormatter getDateFormatter(){
-        String dateFormat = properties.getProperty("dateFormat");
+        String dateFormat = getProperties().getProperty("dateFormat");
         return dateFormat == null ? DateTimeFormatter.ISO_DATE : DateTimeFormatter.ofPattern(dateFormat);
     }
 
-    public static String getSortType(){
-        return properties.getProperty("sortType");
+    public static Comparator<Employee> getComparator(){
+        String sortType = getProperties().getProperty("sortType");
+        stdout.debug("sortType={}", sortType);
+        return "ASC".equals(sortType) ? COMPARATOR_ASC : COMPARATOR_DESC;
     }
 
 }
--- src/main/java/com/infoshare/academy/highfive/SortingType.java
+++ src/main/java/com/infoshare/academy/highfive/SortingType.java
@@ -1,6 +1,10 @@
 package com.infoshare.academy.highfive;
 
+
+
 public enum  SortingType {
-    ASC,
-    DESC
+    //ASC,
+
+
+    //DESC
 }
