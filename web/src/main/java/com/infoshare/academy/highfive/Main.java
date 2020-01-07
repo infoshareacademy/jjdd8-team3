827ad4cfc8cbf2a0a322d63e22ee7673fde887ec
maciej <bandurskim@gmail.com>
2020-01-07 17:00:58 +0100

    Added logging level info and above to file

---
+++ web/src/main/java/com/infoshare/academy/highfive/Main.java
@@ -0,0 +1,17 @@
+package com.infoshare.academy.highfive;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Main {
+
+  private static final Logger LOGGER = LogManager.getLogger(Main.class);
+
+  public static void main(String[] args) {
+    System.out.println("Hello world");
+    LOGGER.info("Hello world");
+    LOGGER.warn("Hello world1");
+  }
+
+
+}
