030f86f7a1257c6de2239c238a8f716d8ff0eac8
Michał Chmielewski <mich.chmielewski@gmail.com>
2020-01-17 16:03:05 +0100

    Json IMPORT from URL and FILE

---
+++ web/src/main/java/mapper/holiday/CalenderDataMapper.java
@@ -0,0 +1,20 @@
+package mapper.holiday;
+
+import com.infoshare.academy.highfive.domain.Holiday;
+
+import javax.ejb.Stateless;
+import javax.json.JsonObject;
+
+
+@Stateless
+public class CalenderDataMapper {
+
+//    public JsonObject toJson(Holiday h) {
+//        return JsonHelper.toJson(
+//                "id", h.getId(),
+//                "name", h.getName(),
+//                "description", h.getDescription(),
+//                "date", h.getDate(),
+//                "type", h.getHolidayType().getType());
+//    }
+}
