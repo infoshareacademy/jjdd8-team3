e7f7b4fb0fa54bf9b3371f05e4e696ee4a90d42b
Michał Chmielewski <mich.chmielewski@gmail.com>
2020-01-17 15:56:54 +0100

    Start of feature ADD CUSTOM Holiday

---
+++ web/src/main/java/com/infoshare/academy/highfive/servlet/holiday/AddHolidayServlet.java
@@ -0,0 +1,74 @@
+package com.infoshare.academy.highfive.servlet.holiday;
+
+import com.infoshare.academy.highfive.domain.HolidayType;
+import com.infoshare.academy.highfive.freemarker.TemplateProvider;
+import com.infoshare.academy.highfive.service.holiday.HolidayService;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.inject.Inject;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+@MultipartConfig
+@WebServlet("/admin/add-holiday")
+public class AddHolidayServlet extends HttpServlet {
+
+    private Logger logger = LoggerFactory.getLogger(getClass().getName());
+
+
+    @Inject
+    private TemplateProvider templateProvider;
+
+    @Inject
+    private HolidayService holidayService;
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=UTF-8");
+        String action = req.getParameter("action");
+        PrintWriter writer = resp.getWriter();
+
+        Map<String, Object> dataModel = new HashMap<>();
+
+        Template template = this.templateProvider.getTemplate(getServletContext(), "template.ftlh");
+
+
+
+        dataModel.put("method", req.getMethod());
+        dataModel.put("contentTemplate", "add-holiday.ftlh");
+        dataModel.put("title", "Add holiday");
+        dataModel.put("action", "add");
+        dataModel.put("holidayTypes", HolidayType.values());
+        dataModel.put("pluginJsTemplate", "plugin-js-add-holiday.ftlh");
+
+        try {
+            template.process(dataModel, writer);
+        } catch (
+                TemplateException e) {
+            e.getMessage();
+        }
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        Map asasasas = req.getParameterMap();
+
+
+        resp.setContentType("text/html;charset=UTF-8");
+        PrintWriter writer = resp.getWriter();
+        resp.setStatus(HttpServletResponse.SC_OK);
+    }
+}
---
+++ web/src/main/java/mapper/holiday/HolidayJsonMapper.java
@@ -0,0 +1,21 @@
+package mapper.holiday;
+
+
+import com.infoshare.academy.highfive.domain.Holiday;
+
+import javax.ejb.Stateless;
+import javax.json.JsonObject;
+
+@Stateless
+public class HolidayJsonMapper {
+
+//    public JsonObject toJson(Holiday h) {
+//        return JsonHelper.toJson(
+//                "id", h.getId(),
+//                "name", h.getName(),
+//                "description", h.getDescription(),
+//                "date", h.getDate(),
+//                "type", h.getHolidayType().getType());
+//    }
+
+}
