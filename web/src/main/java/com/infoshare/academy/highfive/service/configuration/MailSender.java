a661e0d1c5bd25f513af1dbd8fbb350e928486eb
maciej <bandurskim@gmail.com>
2020-01-19 19:16:37 +0100

    Created methods for sending emails.

--- web/src/main/java/com/infoshare/academy/highfive/service/configuration/MailSender.java
+++ web/src/main/java/com/infoshare/academy/highfive/service/configuration/MailSender.java
@@ -17,7 +17,7 @@ public class MailSender {
   private void createMail(Email from, String subject, Email to, Content content) throws IOException {
     Mail mail = new Mail(from, subject, to, content);
 
-    SendGrid sg = new SendGrid(System.getenv("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"));
+    SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
     Request request = new Request();
     request.setMethod(Method.POST);
     request.setEndpoint("mail/send");
