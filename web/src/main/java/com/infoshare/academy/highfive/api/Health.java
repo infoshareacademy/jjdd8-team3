bbb37b607cc631f24bcee285a466b6da1e1e579c
Michał Chmielewski <mich.chmielewski@gmail.com>
2020-01-22 10:58:57 +0100

    Added Health check with status 200

--- web/src/main/java/com/infoshare/academy/highfive/api/Health.java
+++ web/src/main/java/com/infoshare/academy/highfive/api/Health.java
@@ -4,13 +4,14 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
-@Path("/ping")
-public class Ping {
+@Path("/health")
+public class Health {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String get() {
-        return "ping";
+    public Response get() {
+        return Response.status(200).build();
     }
 }
